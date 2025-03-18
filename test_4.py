72d9e845d8f0022bf873b7f1172497144096433b
ElizAnto <elizarov@anton-olegovich.ru>
2025-03-18 10:26:19 +0400

    add scrolling and navigation

--- test_4.py
+++ test_4.py
@@ -1,8 +1,8 @@
-# KEYBOARD
+# Скроллинг и наведение
 
 import time, datetime
 from selenium import webdriver
-from selenium.webdriver import Keys
+from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
@@ -20,33 +20,23 @@ password_all = "secret_sauce"
 user_name = driver.find_element(By.XPATH, "//input[@id='user-name']") #data-test XPATH
 user_name.send_keys(login_standard_user)
 print("Input Login")
-# # Стереть и добавить символы в строку
-# time.sleep(2)
-# user_name.send_keys(Keys.BACKSPACE)
-# time.sleep(2)
-# user_name.send_keys(Keys.BACKSPACE)
-# time.sleep(2)
-# user_name.send_keys("er")
 
 password = driver.find_element(By.XPATH, "//input[@id='password']")
 password.send_keys(password_all)
 print("Input Password")
 
-user_name.send_keys(Keys.RETURN) # Нажатие на клавишу ENTER, минуя кнопку Login
-# button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
-# button_login.click()
-# print("Click Login Button")
+button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
+button_login.click()
+print("Click Login Button")
 
-filter = driver.find_element(By.XPATH, "//select[@data-test='product-sort-container']")
-filter.click()
-print("Click filter")
+# Параметры перемещения по экрану (скроллинг)
+# driver.execute_script("window.scrollTo(0, 500)")
+action = ActionChains(driver)
+red_t_shirt = driver.find_element(By.XPATH, "//button[@id='add-to-cart-test.allthethings()-t-shirt-(red)']")
+action.move_to_element(red_t_shirt).perform()
 time.sleep(2)
-filter.send_keys(Keys.DOWN) # Кнопка на клавиатуре вниз
-time.sleep(2)
-filter.send_keys(Keys.RETURN)
-# Скрин
+
 now_date = datetime.datetime.now().strftime("%Y.%m.%d-%H.%M.%S")
 name_screenshot = "screenshot " + now_date + ".png"
-# ./Screen - точка указывает на текущий каталог, то есть найти в этом проекте папку Screen
 driver.save_screenshot(f".//Screen//{name_screenshot}")
 print("Screenshot done")
