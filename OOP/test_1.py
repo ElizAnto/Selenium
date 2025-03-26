76596f32c2a225d019371f6e5dd491f268ff0458
ElizAnto <elizarov@anton-olegovich.ru>
2025-03-26 18:34:57 +0400

    Add login

--- OOP/test_1.py
+++ OOP/test_1.py
@@ -14,6 +14,21 @@ class Test_1():
 
         print("Start test")
 
+        login_standard_user = "standard_user"
+        password_all = "secret_sauce"
+
+        user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")  # data-test XPATH
+        user_name.send_keys(login_standard_user)
+        print("Input Login")
+
+        password = driver.find_element(By.XPATH, "//input[@id='password']")
+        password.send_keys(password_all)
+        print("Input Password")
+
+        button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
+        button_login.click()
+        print("Click Login Button")
+
         time.sleep(2)
 
 test = Test_1()
