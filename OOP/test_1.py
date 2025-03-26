db43c433da17eb50fbbb382821e11024d5f4b5e6
ElizAnto <elizarov@anton-olegovich.ru>
2025-03-26 19:12:46 +0400

    smoke testing of the entire business path

--- OOP/test_1.py
+++ OOP/test_1.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.service import Service
 import time
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Test_1():
@@ -17,17 +19,31 @@ class Test_1():
         login_standard_user = "standard_user"
         password_all = "secret_sauce"
 
-        user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")  # data-test XPATH
+        user_name = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='user-name']")))
         user_name.send_keys(login_standard_user)
         print("Input Login")
 
-        password = driver.find_element(By.XPATH, "//input[@id='password']")
+        password = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='password']")))
         password.send_keys(password_all)
         print("Input Password")
 
-        button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
+        button_login = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='login-button']")))
         button_login.click()
         print("Click Login Button")
+
+        select_product = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']")))
+        select_product.click()
+        print("Click Select Product")
+
+        enter_shopping_cart = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//div[@id='shopping_cart_container']")))
+        enter_shopping_cart.click()
+        print("Clock Enter Shopping Cart")
+
+        success_test = WebDriverWait(driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[@class='title']")))
+        value_success_test = success_test.text
+        assert value_success_test == 'Your Cart'
+        print("Test Success!")
 
         time.sleep(2)
 
